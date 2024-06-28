227641d3a9ec4fa65e214e85ae4fcd64dfe7d6df
raffraff <raffalimbuyuguen@gmail.com>
2024-06-28 17:53:24 +0800

    Initial commit

---
+++ src/main/java/com/farmcollector/farmcollector/FarmcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.farmcollector.farmcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmcollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmcollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmcollector/farmcollector/FarmcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmcollector.farmcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmcollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
